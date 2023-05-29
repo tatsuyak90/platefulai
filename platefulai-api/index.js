7a309513fa6f23793476581aa8de6d959fce1bae
tatsuyak90 <tkaoru.jp@firemail.cc>
2023-05-29 19:38:48 +0200

    feat: initial setup: an api and a barebones ui that calls the api. at the moment, we just print the generated output to server console.

---
+++ platefulai-api/index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const express = require("express")
+const { AI_PROMPT, Client, HUMAN_PROMPT } = require("@anthropic-ai/sdk")
+const cors = require('cors')
+
+const apiKey = process.env.ANTHROPIC_API_KEY
+
+if (!apiKey) {
+  throw new Error("The ANTHROPIC_API_KEY environment variable must be set")
+}
+
+const claude = new Client(apiKey)
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors());
+
+app.post("/mealPlan", (req, res) => {
+  const input = req.body
+  console.log(mkPrompt(input))
+  claude
+    .complete({
+      prompt: `${HUMAN_PROMPT} ${mkPrompt(input)}${AI_PROMPT}`,
+      stop_sequences: [HUMAN_PROMPT],
+      max_tokens_to_sample: 20000,
+      model: "claude-v1-100k",
+    })
+    .then(({ completion }) => {
+      console.log(completion)
+      res.json({ mealPlan: completion })
+    })
+    .catch((error) => {
+      res.json({ error: "An error occurred. Please contact Team Helix." })
+      console.error(error)
+    })
+
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+const mkPrompt = ({ persons, restrictions, ingredients, days, mealsPerDay, calsPerDay }) => `
+Your task is to suggest a meal plan and shopping list as a JSON string in the following format.
+
+type Output = {
+  // you must include ${days * mealsPerDay} meals
+  meals: Array<{
+    recipeName: string
+    ingredients: string[] // should include quantity
+    instructions: string
+    caloriesPerPerson: number
+  }>,
+  // List of shopping items based on the sum of all ingredients used for the suggested recipes. should include quantity and be enough to cook all suggested recipes.
+  // Items must be in the form you might buy them from the store, e.g.
+  // 2 avocados, NOT "1 avocado, diced"
+  // Remember not to deduce ingredients the user said they already have.
+  shoppingList: string[]
+}
+
+Follow these instructions:
+
+- Each meal must feed ${persons} person(s)
+- The following restrictions were provided: ${restrictions || '[none]'}
+- The user already possesses the following ingredients and is looking to use them: ${ingredients || '[none]'}
+- The total number of meals must be ${days * mealsPerDay}.
+- The daily calorie budget is ${calsPerDay}.
+- Output the valid JSON ONLY, do not write anything else. Include the full requested data.
+`
---
+++ platefulai-fe/src/App.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MealPlanForm />
+    </div>
+  )
+}
+
+const MealPlanForm = () => {
+  const [persons, setPersons] = useState(1)
+  const [days, setDays] = useState(4)
+  const [meals, setMeals] = useState(3)
+  const [calories, setCalories] = useState(2000)
+  const [dietaryRestrictions, setDietaryRestrictions] = useState('')
+  const [ingredients, setIngredients] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const formData = {
+      persons,
+      restrictions: dietaryRestrictions,
+      ingredients,
+      days,
+      mealsPerDay: meals,
+      calsPerDay: calories,
+    }
+
+    fetch('http://localhost:3000/mealPlan', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Response from server:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Number of persons to feed:
+        <select value={persons} onChange={(e) => setPersons(parseInt(e.target.value))}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+        </select>
+      </label>
+      <br />
+
+      <label>
+        Number of days:
+        <select value={days} onChange={(e) => setDays(parseInt(e.target.value))}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+        </select>
+      </label>
+      <br />
+
+      <label>
+        Meals per day:
+        <select value={meals} onChange={(e) => setMeals(parseInt(e.target.value))}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </label>
+      <br />
+
+      <label>
+        Calories per day:
+        <input
+          type="number"
+          value={calories}
+          onChange={(e) => setCalories(parseInt(e.target.value))}
+          min={800}
+          max={4000}
+        />
+      </label>
+      <br />
+
+      <label>
+        Dietary restrictions:
+        <input
+          type="text"
+          value={dietaryRestrictions}
+          onChange={(e) => setDietaryRestrictions(e.target.value)}
+          placeholder="e.g. vegan, seafood allergy, no pork, etc."
+        />
+      </label>
+      <br />
+
+      <label>
+        Ingredients I already have and want to use:
+        <input
+          type="text"
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+          placeholder="e.g. 1 whole chicken, 1kg potatoes"
+        />
+      </label>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default App
